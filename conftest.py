7b67303fe50f666525d473831f437addecf16eab
mysl <xcalibur220@gmail.com>
2020-03-25 14:47:49 -0400

    Create global variable CONVERGE_URL

--- conftest.py
+++ conftest.py
@@ -14,9 +14,16 @@ def browser(request):
     for item in session.items:
         cls = item.getparent(pytest.Class)
         setattr(cls.obj, "driver", driver)
-    driver.get("https://ywamconverge.org/")  # Current
-    # driver.get("https://glenis.ywamconverge.org/")  # L10N by Glenis
-    # driver.get("https://dev02.ywamconverge.org/")  # India
+    global CONVERGE_URL
+    CONVERGE_URL = 'https://ywamconverge.org/'
+    # CONVERGE_URL = 'https://glenis.ywamconverge.org/' # L10N by Glenis
+    # CONVERGE_URL = 'https://dev02.ywamconverge.org/' # India
+    driver.get(CONVERGE_URL)  # Current
     driver.maximize_window()
     yield driver
     driver.quit()
+
+# Set global variable to return to homepage
+@pytest.fixture(scope='session')
+def api_url():
+    return CONVERGE_URL
