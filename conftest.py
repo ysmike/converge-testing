e6737a1f7e8b0434d0942ca2c7150cf99ef7d48c
mysl <xcalibur220@gmail.com>
2020-04-09 19:39:09 -0400

    Add --env argparse & connect with global variable

--- conftest.py
+++ conftest.py
@@ -1,28 +1,39 @@
 import pytest
 from selenium import webdriver
 
+# Pass args to determine the test environment: prod, dev, or india
+# Set to "dev" by default
+# ex) "pytest --env prod"
+# TODO: Add args for selecting different browsers
+def pytest_addoption(parser):
+    parser.addoption(
+        "--env", action="store", default="dev", help="env options: prod, dev, india"
+    )
+
+
+@pytest.fixture(scope="session")
+def env(request):
+    return request.config.getoption("--env")
+
+
+@pytest.fixture(scope="session")
+def api_url(env):
+    if env == "dev":
+        return "https://glenis.ywamconverge.org/"  # L10N by Glenis
+    elif env == "prod":
+        return "https://ywamconverge.org/"  # Prod
+    elif env == "india":
+        return "https://dev02.ywamconverge.org/"  # India
+
 
 # Create a fixture as to use a single browser for the session
 @pytest.fixture(scope="session")
 def browser(request):
-    # TODO: Add args for selecting different/(if possible all) browsers
     driver = webdriver.Chrome("./chromedriver")
     session = request.node
     for item in session.items:
         cls = item.getparent(pytest.Class)
         setattr(cls.obj, "driver", driver)
-    global CONVERGE_URL
-    CONVERGE_URL = "https://ywamconverge.org/"
-    # CONVERGE_URL = "https://glenis.ywamconverge.org/"  # L10N by Glenis
-    # CONVERGE_URL = "https://dev02.ywamconverge.org/"  # India
-    driver.get(CONVERGE_URL)  # Current
     driver.maximize_window()
     yield driver
     driver.quit()
-
-
-# Set a global variable to return to homepage
-# TODO: Change global variable to work with xdist
-@pytest.fixture(scope="session")
-def api_url():
-    return CONVERGE_URL
