484b231661fc7412ecdd300609a9a49f24886dad
mysl <xcalibur220@gmail.com>
2020-04-09 19:40:25 -0400

    Start at homepage using the global variable

--- tests/test_search_internships.py
+++ tests/test_search_internships.py
@@ -6,12 +6,20 @@ import pytest
 
 from selenium.webdriver.common.by import By
 
+
 @pytest.mark.usefixtures("browser")
 class TestSearchInterships:
+    def test_go_to_homepage(self, api_url):
+        self.driver.get(api_url)
+
     def test_internship_text(self):
-        search_link = self.driver.find_element(By.LINK_TEXT, "Check out the internships")
+        search_link = self.driver.find_element(
+            By.LINK_TEXT, "Check out the internships"
+        )
         search_link.click()
-        total_text = self.driver.find_element(By.CLASS_NAME, "total-projects").text[-17:]
+        total_text = self.driver.find_element(By.CLASS_NAME, "total-projects").text[
+            -17:
+        ]
         filter_text = self.driver.find_element(By.CSS_SELECTOR, ".filter div h3").text
         assert total_text == "Total Internships" and filter_text == "Advanced Filters"
         # ! self.driver.back() not necessary when using pytest-xdist
